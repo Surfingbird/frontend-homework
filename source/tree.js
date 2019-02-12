832138e4e5d6cd8c9b3763b8030d1cbf57bd4e62
a.baronskiy <a.baronskiy@mail.ru>
2019-02-12 12:13:48 +0300

    стрелочный оператор, const переменные и заменил конкатенацию на шаблон

--- source/tree.js
+++ source/tree.js
@@ -1,36 +1,30 @@
 'use strict';
 
 function isCorrectVal(n) {
-    let parseVal = parseFloat(n);
+    const parseVal = parseFloat(n);
 
     return !isNaN(parseVal) && isFinite(n) && (~~parseVal === parseVal);
 }
 
-function tree(depth) {
+const tree = (depth) => {
     const minDepth = 3;
 
     if (!isCorrectVal(depth) || depth < minDepth) {
         return null;
     }
 
-    let freeSpace = depth - 2;
-    let resultParts = [];
+    const freeSpace = depth - 2;
+    const resultParts = [];
     let starsCount = 1;
 
     for (let i = 1, localFreeSpace = freeSpace; i < depth; i++) {
-
-        resultParts.push(" ".repeat(localFreeSpace) +
-            "*".repeat(starsCount) +
-            " ".repeat(localFreeSpace) + "\n");
+        resultParts.push(`${" ".repeat(localFreeSpace)}${"*".repeat(starsCount)}${" ".repeat(localFreeSpace)}\n`);
 
         starsCount += 2;
         --localFreeSpace;
     }
 
-    resultParts.push(" ".repeat(freeSpace) +
-        "|" +
-        " ".repeat(freeSpace) + "\n");
+    resultParts.push(`${" ".repeat(freeSpace)}|${" ".repeat(freeSpace)}\n`);
 
     return resultParts.join("");
-
-}
+};
